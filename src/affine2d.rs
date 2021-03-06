21d30cb766a3d62081bbe107063c09bfcc4b4daf
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-03-06 22:47:13 +0100

    Small format change

--- src/affine2d.rs
+++ src/affine2d.rs
@@ -3,9 +3,9 @@ use nalgebra::{Matrix3, Vector6};
 #[rustfmt::skip]
 pub fn projection_mat(params: &Vector6<f32>) -> Matrix3<f32> {
     Matrix3::new(
-        1.0 + params[0], params[2], params[4],
-        params[1], 1.0 + params[3], params[5],
-        0.0, 0.0, 1.0,
+        1.0 + params[0],       params[2], params[4],
+              params[1], 1.0 + params[3], params[5],
+                    0.0,             0.0,       1.0,
     )
 }
 
