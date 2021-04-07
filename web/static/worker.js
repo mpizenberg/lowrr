72f1325c4ceea871f1d493197364148697b0ade4
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-04-07 15:08:07 +0200

    Cropping images in a webworker

--- web/static/worker.js
+++ web/static/worker.js
@@ -4,21 +4,61 @@
 
 console.log("Hello from worker");
 
-let images = [];
+const images = [];
+const croppedImages = [];
+// TODO: Temporary, chrome-only
+const canvas = new OffscreenCanvas(100, 100);
+const ctx = canvas.getContext("2d");
 
 // Listener for messages containing data of the shape: { type, data }
 // where type can be one of:
 //   - "image-loaded": received when an image was loaded in the main thread
-onmessage = function (event) {
+onmessage = async function (event) {
   console.log(`worker message: ${event.data.type}`);
   if (event.data.type == "image-loaded") {
     images.push(event.data.data);
   } else if (event.data.type == "run") {
-    run(event.data.data);
+    await run(event.data.data);
   }
 };
 
 // Main algorithm with the parameters passed as arguments.
-function run(params) {
+async function run(params) {
   console.log("worker running with parameters:", params);
+  // Reshape the canvas to the appropriate cropped size.
+  if (params.crop != null) {
+    canvas.width = params.crop.right - params.crop.left;
+    canvas.height = params.crop.right - params.crop.left;
+  } else {
+    canvas.width = images[0].width;
+    canvas.height = images[0].height;
+  }
+  // Crop all images
+  let x, y;
+  if (params.crop == null) {
+    x = 0;
+    y = 0;
+  } else {
+    x = params.crop.left;
+    y = params.crop.top;
+  }
+  let width = canvas.width;
+  let height = canvas.height;
+  for (let img of images) {
+    let croppedImg = await crop(img, x, y, width, height);
+    croppedImages.push(croppedImg);
+  }
+}
+
+// Temporary function just to crop a given image.
+async function crop(img, x, y, width, height) {
+  console.log("Cropping image ", img);
+  const response = await fetch(img.url);
+  const data = await response.blob();
+  const imgBitmap = await createImageBitmap(data, x, y, width, height);
+  ctx.drawImage(imgBitmap, 0, 0);
+  imgBitmap.close();
+  const croppedBlob = await canvas.convertToBlob();
+  const croppedUrl = URL.createObjectURL(croppedBlob);
+  return { id: img.id, url: croppedUrl, width, height };
 }
